c045e906cdbe33b847803d5c84791a6f556a88e1
Jeremy Jones <JeremyJones@QuickenLoans.com>
2021-11-08 16:14:30 -0500

    Started DAL

---
+++ DB6_Capstone_G3/Models/DAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using MySql.Data.MySqlClient;
+using DB6_Capstone_G3.Models;
+
+namespace DB6_Capstone_G3.Models
+{
+    public class DAL
+    {
+        public static MySqlConnection Database;
+        private static HttpClient client = null;
+        private static HttpClient GetHttpClient()
+        {
+            if (client is null)
+            {
+                client = new HttpClient();
+                client.BaseAddress = new Uri("https://www.thecocktaildb.com/");
+            }
+
+            return client;
+        }
+
+        public async static Task<Cocktail> GetCocktailByName(string userSearch)
+        {
+            var connection = await GetHttpClient().GetAsync($"www.thecocktaildb.com/api/json/v1/1/search.php?s={userSearch}");
+            Cocktail response = await connection.Content.ReadAsAsync<Cocktail>();
+            return response;
+        }
+
+        public async static Task<Cocktail[]> GetCocktailsByIngredient(string userSearch)
+        {
+            var connection = await GetHttpClient().GetAsync($"www.thecocktaildb.com/api/json/v1/1/filter.php?i={userSearch}");
+            Cocktail[] response = await connection.Content.ReadAsAsync<Cocktail[]>();
+            return response;
+        }
+    }
+}
