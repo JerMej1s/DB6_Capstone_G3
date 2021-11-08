11468cd408ab43c6d802f7986e63b03367812575
Brandon Miller <Brandonmiller1@quickenloans.com>
2021-11-08 14:59:32 -0500

    Added classes

--- DB6_Capstone_G3/Models/CockTailClass.cs
+++ DB6_Capstone_G3/Models/CockTailClass.cs
@@ -8,7 +8,7 @@ using Dapper.Contrib.Extensions;
 namespace DB6_Capstone_G3.Models
 {
     [Table("cocktail")]
-    public class CockTailClass
+    public class CocktailClass
     {
         [Key]
         int idDrink { get; set; }
