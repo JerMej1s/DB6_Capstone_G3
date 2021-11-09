95bbbe16df5621c3296d6d56307e55eb73b5b926
Jeremy Jones <JeremyJones@QuickenLoans.com>
2021-11-09 18:16:12 -0500

    Created MealsController and added random cocktail search

---
+++ DB6_Capstone_G3/Controllers/MealsController.cs
@@ -0,0 +1,39 @@
+﻿using DB6_Capstone_G3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DB6_Capstone_G3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MealsController : ControllerBase
+    {
+        [HttpGet("{mealName}")]
+        public async Task<MealResponse> SearchMealByName(string mealName)
+        {
+            return await DAL.GetMealsByName(mealName);
+        }
+
+        [HttpGet("ingredient/{ingredient}")]
+        public async Task<MealResponse> SearchMealByIngredient(string ingredient)
+        {
+            return await DAL.GetMealsByIngredient(ingredient);
+        }
+
+        [HttpGet("letter/{firstLetterSearch}")]
+        public async Task<MealResponse> SearchMealByFirstLetter(char firstLetterSearch)
+        {
+            return await DAL.GetMealsByFirstLetter(firstLetterSearch);
+        }
+
+        [HttpGet("random")]
+        public async Task<MealResponse> GetRandomMeal()
+        {
+            return await DAL.GetRandomMeal();
+        }
+    }
+}
