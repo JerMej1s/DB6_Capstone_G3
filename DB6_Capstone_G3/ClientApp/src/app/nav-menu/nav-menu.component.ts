891f3a240e3c90aadf99e0f23d1c371334ded07f
Brandon Miller <Brandonmiller1@quickenloans.com>
2021-11-18 18:48:29 -0500

    Changing back nav menu

--- DB6_Capstone_G3/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ DB6_Capstone_G3/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,27 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AuthService } from '../auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent implements OnInit {
-  public currentIdUser?: Observable<number>
-
-  constructor(private auth: AuthService) { }
-
-  idUser: number = -1;
-
-  ngOnInit(): void {
-    this.currentIdUser = this.auth.getCurrentUserId();
-
-    this.currentIdUser.subscribe((idUser: number) => {
-      console.log(`Logging from nav Component in events: ${idUser}`);
-      this.idUser = idUser;
-    })
-  }
+export class NavMenuComponent {
 
   isExpanded = false;
 
